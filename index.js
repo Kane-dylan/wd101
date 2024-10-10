fa220c41b37b6a5725075aa2c7df298c8c4b32e4
kirandas <cool.kdtech1234@gmail.com>
2024-10-10 13:32:34 +0530

    form creater

--- index.js
+++ index.js
@@ -1,60 +1,20 @@
-document.addEventListener("DOMContentLoaded",()=>{
-  const cityInput=document.getElementById("city-input")
-  const getWeatherBTN=document.getElementById("get-weather-btn")
-  const weatherInfo=document.getElementById("weather-info")
-  const cityNameDisplay=document.getElementById("city-name")
-  const temperatureDisplay=document.getElementById("temperature")
-  const descriptionDisplay = document.getElementById("description");
-  const errorMessage = document.getElementById("error-message");
+document
+  .getElementById("userForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const dob = document.getElementById("dob").value;
+    const acceptedTerms = document.getElementById("terms").checked;
 
-  const API_KEY = "ad754cfac320391d7e26d52ae82d3b6e";
+    const tableBody = document.getElementById("userTableBody");
+    const newRow = tableBody.insertRow();
+    newRow.insertCell(0).textContent = name;
+    newRow.insertCell(1).textContent = email;
+    newRow.insertCell(2).textContent = password;
+    newRow.insertCell(3).textContent = dob;
+    newRow.insertCell(4).textContent = acceptedTerms ? "true" : "false";
 
-  getWeatherBTN.addEventListener("click",async()=>{
-    const city=cityInput.value.trim()
-    if(!city)return
-    
-    //it may through some error 
-    //server/db is always in another continent
-
-    try {
-      const weatherData=await fetchWeatherData(city)
-      displayWeatherData(weatherData)
-    } catch (error) {
-      showError()
-    }
-    
-
-  })
-
-  async function fetchWeatherData(city){
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
-
-    const response = await fetch(url)
-    console.log(response);
-
-    if(!response.ok){
-      throw new Error(data.message || "Something went wrong")
-    }
-
-    const data=await response.json()
-    return data
-  }
-
-  function displayWeatherData(data){
-  
-    console.log(data);
-    const {name, main, weather}= data
-    cityNameDisplay.textContent=name
-    temperatureDisplay.textContent=`Temperature: ${main.temp}°C`
-    descriptionDisplay.textContent=`Description: ${weather[0].description}`
-
-    //unhide the weather inf
-    weatherInfo.classList.remove("hidden")
-    errorMessage.classList.add("hidden")
-  }
-
-  function showError(){
-    weatherInfo.classList.remove("hidden")
-    errorMessage.classList.add("hidden")
-  }
-})
+    document.getElementById("userForm").reset();
+  });
